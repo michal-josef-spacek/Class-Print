55bde526869da1232292429bef11da43a9e3f2d3
Michal Špaček <tupinek@gmail.com>
2013-05-26 21:06:59 +0200

    Add example.

--- Print/Utils.pm
+++ Print/Utils.pm
@@ -179,9 +179,33 @@ Class::Print::Utils - Class::Print utilities.
  use Indent;
  use Class::Print::Utils qw(print_value);
 
- # TODO
+ package Object;
+
+ # Constructor.
+ sub new {
+         return bless { 'id' => '0001' }, 'Object';
+ }
+
+ # Id.
+ sub id {
+         my $self = shift;
+         return $self->{'id'}; 
+ }
+
+ package main;
+
+ # Create object.
+ my $obj = Object->new;
+
+ # Print out.
+ print print_value('scalar')."\n";
+ print print_value([1, 2])."\n";
+ print print_value($obj)."\n";
 
  # Output.
+ # scalar
+ # 1, 2
+ # 0001
 
 =head1 DEPENDENCIES
 
--- examples/ex3.pl
+++ examples/ex3.pl
@@ -8,6 +8,28 @@ use warnings;
 use Indent;
 use Class::Print::Utils qw(print_value);
 
-# TODO
+package Object;
+
+# Constructor.
+sub new {
+        return bless { 'id' => '0001' }, 'Object';
+}
+
+# Id.
+sub id {
+        my $self = shift;
+        return $self->{'id'}; 
+}
+
+package main;
+
+# Create object.
+my $obj = Object->new;
+
+# Print out.
+print print_value('scalar')."\n";
+print print_value([1, 2])."\n";
+print print_value($obj);
 
 # Output.
+# TODO
