06517401eee309206fa3bdd03e6c74ebe482ff34
Michal Špaček <tupinek@gmail.com>
2013-06-13 11:34:16 +0200

    Add EXAMPLE2 to doc.

--- Print.pm
+++ Print.pm
@@ -106,7 +106,7 @@ Class::Print - Perl class for helping of object information print.
  print():
          Bad Indent object.
 
-=head1 EXAMPLE
+=head1 EXAMPLE1
 
  # Pragmas.
  use strict;
@@ -129,6 +129,35 @@ Class::Print - Perl class for helping of object information print.
 
  # Output:
  # Identification: 0001
+
+=head1 EXAMPLE2
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Class::Print;
+ use Dumpvalue;
+
+ # Object.
+ my $obj = Class::Print->new;
+
+ # Example class.
+ my $example = bless { 'id' => '0001' }, 'Example';
+
+ # Get output.
+ my @ret = $obj->print($example, [
+         ['id', 'Identification'],
+ ]);
+
+ # Dump out.
+ my $dump = Dumpvalue->new;
+ $dump->dumpValues(\@ret);
+
+ # Output:
+ # 0  ARRAY(0x2580d00)
+ #   0  'Identification: 0001'
 
 =head1 DEPENDENCIES
 
